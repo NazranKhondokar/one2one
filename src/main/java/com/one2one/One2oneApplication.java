e3e02cc559bacf6e56fe6110a3d63563b6287605
nazran <nazran@eatlbd.com>
2022-02-09 10:08:38 +0600

    aws demo

--- src/main/java/com/one2one/One2oneApplication.java
+++ src/main/java/com/one2one/One2oneApplication.java
@@ -2,9 +2,17 @@ package com.one2one;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class One2oneApplication {
+
+	@GetMapping("/")
+	public String home() {
+		return "Welcome to One2One";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(One2oneApplication.class, args);
